e8acb70a225445bad9108e1d1e93fb958e47d7ec
Ахмэд Степанов <iostepanovk@gmail.com>
2017-12-14 16:59:10 +0500

    Refactor

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -4,33 +4,58 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float TurnSpeed = 0.3f; 
+    #region SingletoneBody
+    
+    private static CameraController _instance;
+    public static CameraController GetInstance()
+    {
+        if (_instance == null)
+            _instance = new CameraController();
+        return _instance;
+    }
+    private CameraController()
+    {
+
+    }
+    #endregion
+    public float TurnSpeed = 0.3f;
     public Transform ParentTransform;
-    private float _prePosX;    
-    private bool _isRotating; 
-	void Update ()
-	{
-	    if (_isRotating)
-	    {
-	        float Delta = Input.mousePosition.x - _prePosX;
+    private float _prePosX;
+    private bool _isRotating;
+    void Start()
+    {
+        _instance = this;
+    }
+
+    void Update()
+    {
+        if (_isRotating)
+        {
+            float Delta = Input.mousePosition.x - _prePosX;
             ParentTransform.RotateAround(ParentTransform.position, Vector3.up, Delta * TurnSpeed);
             _prePosX = Input.mousePosition.x;
         }
-	    if (Input.GetMouseButton(0) && !_isRotating)
-	    {
-	        _isRotating = true;
-	        _prePosX = Input.mousePosition.x;
-	    }
-	    else
-	    {
-	        _isRotating = false;
+        if (Input.GetMouseButton(0) && !_isRotating)
+        {
+            _isRotating = true;
+            _prePosX = Input.mousePosition.x;
+        }
+        else
+        {
+            _isRotating = false;
 
-	    }
+        }
     }
-    /*
-    public Coroutine ChangeDistance()
-    {
 
-        
-    }*/
+    public IEnumerator ChangeDistance(Vector3 newPos, float newDistance)
+    {
+        var move = (newPos - ParentTransform.position) / 20;
+        var dis = (newDistance - Vector3.Distance(ParentTransform.position, newPos)) / 20;
+        var pos = ParentTransform.position;
+        for (int i = 0; i < 20; i++)
+        {
+            ParentTransform.position += move;
+            yield return new WaitForSeconds(.01f);
+        }
+    }
 }
